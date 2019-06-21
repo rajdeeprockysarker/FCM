959e26d11b9d4a84b6212d1972ffab52b6e29fca
rajdeeprockysarker <rajdeepsarker1986@gmail.com>
2019-06-21 09:12:13 +0530

    Initial commit

---
+++ app/src/main/java/com/raj/fcmnew/MainActivity.java
@@ -0,0 +1,25 @@
+package com.raj.fcmnew;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel=
+                    new NotificationChannel("MyNotifications","MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/raj/fcmnew/MyMessagingService.java
@@ -0,0 +1,52 @@
+package com.raj.fcmnew;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+
+    }
+
+    public void showNotification(String title,String message){
+
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this,"MyNotifications")
+                        .setContentTitle("Notifications Example")
+                        .setContentText("This is a test notification");
+
+        NotificationManagerCompat managerCompat=NotificationManagerCompat.from(this);
+        managerCompat.notify(999,builder.build());
+
+
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = "Successfull";
+                        if (!task.isSuccessful()) {
+                            msg = "Faield";
+                        }
+
+                        Toast.makeText(MyMessagingService.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+    }
+}
